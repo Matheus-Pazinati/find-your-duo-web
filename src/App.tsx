16e4bb04547fec548ca6bbe5a754f1b820622a2c
Matheus Pazinati <matheuspazinati@gmail.com>
2022-09-15 06:08:55 -0300

    Busca dados dos games na api e exibe em tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import { MagnifyingGlassPlus } from 'phosphor-react'
+import { useEffect, useState } from 'react';
+
+import { GameCard } from './components/GameCard';
 
 import './styles/main.css';
 import logoImg from '/logo-nlw-esports.svg';
 
+interface GameProps {
+  baseUrl: string;
+  title: string;
+  _count: {
+    ads: number;
+  }
+}
+
 function App() {
+
+  const [games, setGames] = useState<GameProps[]>([])
+
+  useEffect(() => {
+    async function getGamesFromServer() {
+      const response = await fetch('http://localhost:3333/games')
+      const games = await response.json()
+
+      setGames(games)
+    }
+    getGamesFromServer()
+  }, [])
 
   return (
     <div className='max-w-[1344px] my-20 mx-auto flex flex-col items-center'>
@@ -17,48 +40,9 @@ function App() {
         </h1>
 
         <div className='grid grid-cols-6 gap-6 mt-16'>
-          <a href="#" className='rounded-lg relative overflow-hidden'>
-            <img src="/game-1.png" alt="" />
-            <div className='w-full px-4 pt-16 pb-4 absolute bottom-0 left-0 right-0 bg-game'>
-              <strong className='text-white font-bold block'>League of Legends</strong>
-              <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-            </div>
-          </a>
-          <a href="#" className='rounded-lg relative overflow-hidden'>
-            <img src="/game-2.png" alt="" />
-            <div className='w-full px-4 pt-16 pb-4 absolute bottom-0 left-0 right-0 bg-game'>
-              <strong className='text-white font-bold block'>Dota 2</strong>
-              <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-            </div>
-          </a>
-          <a href="#" className='rounded-lg relative overflow-hidden'>
-            <img src="/game-3.png" alt="" />
-            <div className='w-full px-4 pt-16 pb-4 absolute bottom-0 left-0 right-0 bg-game'>
-              <strong className='text-white font-bold block'>CS:GO</strong>
-              <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-            </div>
-          </a>
-          <a href="#" className='rounded-lg relative overflow-hidden'>
-            <img src="/game-4.png" alt="" />
-            <div className='w-full px-4 pt-16 pb-4 absolute bottom-0 left-0 right-0 bg-game'>
-              <strong className='text-white font-bold block'>Apex Legends</strong>
-              <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-            </div>
-          </a>
-          <a href="#" className='rounded-lg relative overflow-hidden'>
-            <img src="/game-5.png" alt="" />
-            <div className='w-full px-4 pt-16 pb-4 absolute bottom-0 left-0 right-0 bg-game'>
-              <strong className='text-white font-bold block'>Fortnite</strong>
-              <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-            </div>
-          </a>
-          <a href="#" className='rounded-lg relative overflow-hidden'>
-            <img src="/game-6.png" alt="" />
-            <div className='w-full px-4 pt-16 pb-4 absolute bottom-0 left-0 right-0 bg-game'>
-              <strong className='text-white font-bold block'>World of Warcraft</strong>
-              <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-            </div>
-          </a>
+          {games.map((game) => {
+            return <GameCard baseUrl={game.baseUrl} title={game.title} ads={game._count.ads} />
+          })}
         </div>
       </div>
 
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,17 +1,17 @@
 interface GameCardProps {
-  bannerUrl: string;
+  baseUrl: string;
   title: string;
-  adsCount: number;
+  ads: number
 }
 
-export function GameCard({bannerUrl, title, adsCount}: GameCardProps) {
-  const justOneAd = adsCount === 1
+export function GameCard({baseUrl, title, ads}: GameCardProps) {
+  const justOneAd = ads === 1
   return (
     <a href="#" className='rounded-lg relative overflow-hidden'>
-      <img src={bannerUrl} alt={`Banner do jogo ${title}`} />
+      <img src={baseUrl} alt={`Banner do jogo ${title}`} />
       <div className='w-full px-4 pt-16 pb-4 absolute bottom-0 left-0 right-0 bg-game'>
         <strong className='text-white font-bold block'>{title}</strong>
-        <span className='text-zinc-300 text-sm block'>{justOneAd ? `${adsCount} anúncio` : `${adsCount} anúncios` }</span>
+        <span className='text-zinc-300 text-sm block'>{justOneAd ? `${ads} anúncio` : `${ads} anúncios` }</span>
       </div>
     </a>
   )
